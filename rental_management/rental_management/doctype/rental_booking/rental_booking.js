682953cecf4da1546b56415b7db5a09a48bb21e5
“Fadil <fadilsiddique@gmail.com>
2023-05-14 09:28:27 +0000

    feat: rental management

---
+++ rental_management/rental_management/doctype/rental_booking/rental_booking.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Fadil and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Rental Booking', {
+	// refresh: function(frm) {
+
+	// }
+});
