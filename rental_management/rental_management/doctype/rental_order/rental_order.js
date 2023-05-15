1523bdeb92b5bcb1428c7cc9b5f9e327f835aeef
“Fadil <fadilsiddique@gmail.com>
2023-05-15 13:53:50 +0000

    feat: create sales invoice from rental order, redirection from rental order

--- rental_management/rental_management/doctype/rental_order/rental_order.js
+++ rental_management/rental_management/doctype/rental_order/rental_order.js
@@ -3,12 +3,39 @@
 
 frappe.ui.form.on('Rental Order', {
     refresh: function(frm) {
+
+		let items = []
+
+		frm.doc.rental_items.map((event)=>{
+			let item = {
+				'item_code':event.item,
+				'qty':event.quantity
+			}
+			items.push(item)
+		})
+		let data = {
+			'customer':frm.doc.customer,
+			'rental_order_id':frm.doc.name,
+			'items':items
+		}
         frm.add_custom_button(__('Create Sales Invoice'), function() {
+			let salesInvoiceId = ''
 
-        console.log('hello')});	
+			frappe.call({
+				method:'rental_management.rental_management.doctype.rental_order.rental_order.make_sales_invoice',
+				args:{
+					data:data
+				},
+				callback:(res)=>{
+					salesInvoiceId = res.message.name
+					frappe.set_route('Form','Sales Invoice',salesInvoiceId)
+				}
+			})
+				
 
+    });
 
-        // Add more buttons here
+   // Add more buttons here
 
     }
 });
--- rental_management/rental_management/doctype/rental_order/rental_order.py
+++ rental_management/rental_management/doctype/rental_order/rental_order.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2023, Fadil and contributors
 # For license information, please see license.txt
-
-# import frappe
+import json
+import frappe
 from frappe.model.document import Document
 
 class RentalOrder(Document):
 	pass
+
+@frappe.whitelist()
+
+def make_sales_invoice(data):
+	data=json.loads(data)
+	print(data['items'])
+	new_invoice=frappe.get_doc({
+		'doctype':'Sales Invoice',
+		'customer':data['customer'],
+		'rental_order_id':data['rental_order_id'],
+		'items':data['items']
+	})
+
+	new_invoice.insert(ignore_permissions=True)
+	new_invoice.save(ignore_permissions=True)
+
+	return new_invoice
